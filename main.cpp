b23987148896cb0377809e75575a2e4ffaa9d1fe
TeacherUsxKafia <150797793+TeacherUsxKafia@users.noreply.github.com>
2026-05-05 09:45:17 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace Microsoft::WRL;
 HWND hWnd;
 static ComPtr<ICoreWebView2> webviewWindow;
 
-// The Pico++ Mouse Hook Logic
+// THE PICO++ SECRET: Low-Level Mouse Hook for instant firing
 LRESULT CALLBACK MouseHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
     if (nCode == HC_ACTION && webviewWindow) {
         if (wParam == WM_LBUTTONDOWN) {
@@ -25,15 +25,12 @@ LRESULT CALLBACK MouseHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
 }
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow) {
-    // Register Window Class
     WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_HREDRAW | CS_VREDRAW, DefWindowProc, 0, 0, hInst, NULL, LoadCursor(NULL, IDC_ARROW), NULL, NULL, L"KrunkerClass", NULL };
     RegisterClassEx(&wc);
 
-    // Create the Window
     hWnd = CreateWindow(L"KrunkerClass", L"Open Krunker Native", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, NULL, NULL, hInst, NULL);
     ShowWindow(hWnd, nCmdShow);
 
-    // Initialize WebView2 Runtime
     CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
         Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
             [](HRESULT res, ICoreWebView2Environment* env) -> HRESULT {
@@ -41,11 +38,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow) {
                     [](HRESULT res, ICoreWebView2Controller* controller) -> HRESULT {
                         if (controller) {
                             controller->get_CoreWebView2(&webviewWindow);
-                            
-                            // Load the Game
                             webviewWindow->Navigate(L"https://krunker.io");
-                            
-                            // Start the Mouse Hook
                             SetWindowsHookEx(WH_MOUSE_LL, MouseHookProc, GetModuleHandle(NULL), 0);
                         }
                         return S_OK;
@@ -53,7 +46,6 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow) {
                 return S_OK;
             }).Get());
 
-    // Main Message Loop
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
